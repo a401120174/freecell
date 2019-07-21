91b55c4300f9d1d9074616b8e8aa1914b43b3212
a401120174 <a401120174@gmail.com>
2019-07-21 18:15:55 +0800

    Updates

---
+++ precache-manifest.de65658c08d49efa789c344d0dad296d.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee6cdefc09d93a64061031be1d81dd1",
+    "url": "/F2E-WEEK2/index.html"
+  },
+  {
+    "revision": "3ddf63094fac1e5fd4cd",
+    "url": "/F2E-WEEK2/static/css/main.bde5969e.chunk.css"
+  },
+  {
+    "revision": "12f999a07aaaa95b7057",
+    "url": "/F2E-WEEK2/static/js/2.8117fa81.chunk.js"
+  },
+  {
+    "revision": "3ddf63094fac1e5fd4cd",
+    "url": "/F2E-WEEK2/static/js/main.4cad828b.chunk.js"
+  },
+  {
+    "revision": "0e0bb956f6ba009dec5a",
+    "url": "/F2E-WEEK2/static/js/runtime~main.ef77d9e9.js"
+  },
+  {
+    "revision": "5c4a605f4fc51ca9d5e212b018856913",
+    "url": "/F2E-WEEK2/static/media/Congratulations-pic.5c4a605f.png"
+  },
+  {
+    "revision": "b61a4ddd4b02e9351442e74cdbaeb725",
+    "url": "/F2E-WEEK2/static/media/bg-JQK.b61a4ddd.svg"
+  },
+  {
+    "revision": "903e94a633280582422c0176983f8620",
+    "url": "/F2E-WEEK2/static/media/bg-all.903e94a6.png"
+  },
+  {
+    "revision": "43b4d731002b9e80208768d2e91a5546",
+    "url": "/F2E-WEEK2/static/media/bg-left.43b4d731.svg"
+  },
+  {
+    "revision": "ecca7fa07888e5e41adc2b58148d9f96",
+    "url": "/F2E-WEEK2/static/media/bg-right.ecca7fa0.svg"
+  },
+  {
+    "revision": "3d5410c4b01bc5376ab08361b189380d",
+    "url": "/F2E-WEEK2/static/media/bg.3d5410c4.png"
+  },
+  {
+    "revision": "7e3aa11eacde5ec3f16ae90e31b958a5",
+    "url": "/F2E-WEEK2/static/media/btn-more.7e3aa11e.svg"
+  },
+  {
+    "revision": "b63b703c2deac7ac5b28cc1293a3bc66",
+    "url": "/F2E-WEEK2/static/media/btn-return.b63b703c.svg"
+  },
+  {
+    "revision": "6f9e639cd48ff665901492a6e3a6cc1a",
+    "url": "/F2E-WEEK2/static/media/icon-cancel.6f9e639c.svg"
+  },
+  {
+    "revision": "2e575a6230f8d675d25688be94003964",
+    "url": "/F2E-WEEK2/static/media/logo.2e575a62.svg"
+  },
+  {
+    "revision": "c9c8ef0ec9d09902af4a6d43d961c2e8",
+    "url": "/F2E-WEEK2/static/media/愛心11.c9c8ef0e.png"
+  },
+  {
+    "revision": "be3296c4855f934946d078478db31649",
+    "url": "/F2E-WEEK2/static/media/愛心12.be3296c4.png"
+  },
+  {
+    "revision": "7ea88201883560e13f5bcc44d26f06d2",
+    "url": "/F2E-WEEK2/static/media/愛心13.7ea88201.png"
+  },
+  {
+    "revision": "e713937b518d957cb748992baf0f8780",
+    "url": "/F2E-WEEK2/static/media/方塊11.e713937b.png"
+  },
+  {
+    "revision": "b0a8a1a376b09eecc60c42e86b0022f4",
+    "url": "/F2E-WEEK2/static/media/方塊12.b0a8a1a3.png"
+  },
+  {
+    "revision": "5f124f05af635261395c62cdf87a9d9f",
+    "url": "/F2E-WEEK2/static/media/方塊13.5f124f05.png"
+  },
+  {
+    "revision": "99039dbe6ff5eb9620f28253cd882e22",
+    "url": "/F2E-WEEK2/static/media/梅花11.99039dbe.png"
+  },
+  {
+    "revision": "35cea76e179da611f142b12672aad7b1",
+    "url": "/F2E-WEEK2/static/media/梅花12.35cea76e.png"
+  },
+  {
+    "revision": "62878c8ba300cf63bcc5572b09317e21",
+    "url": "/F2E-WEEK2/static/media/梅花13.62878c8b.png"
+  },
+  {
+    "revision": "c1e9817e5d8230a6eaeab7ba5b70c8b5",
+    "url": "/F2E-WEEK2/static/media/骨頭icon.c1e9817e.svg"
+  },
+  {
+    "revision": "c1e2e178d04cdebf6ea3281e5c4e0919",
+    "url": "/F2E-WEEK2/static/media/黑桃11.c1e2e178.png"
+  },
+  {
+    "revision": "9c935ab9fdad8e858ddde49f392945c7",
+    "url": "/F2E-WEEK2/static/media/黑桃12.9c935ab9.png"
+  },
+  {
+    "revision": "3e016a3a1a284c5eaca9e46378f312f8",
+    "url": "/F2E-WEEK2/static/media/黑桃13.3e016a3a.png"
+  }
+]);
